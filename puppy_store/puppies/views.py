f44b52378fad87e819c26fbdc922532d86fed0fd
darkVanci <1585781755@qq.com>
2024-04-30 22:44:25 +0800

    finish tutor

--- puppy_store/puppies/views.py
+++ puppy_store/puppies/views.py
@@ -25,7 +25,6 @@ def get_delete_update_puppy(request, pk):
     elif request.method == 'PUT':
         return Response({})
 
-
 @api_view(['GET', 'POST'])
 def get_post_puppies(request):
     # get all puppies
@@ -46,3 +45,73 @@ def get_delete_update_puppy(request, pk):
     if request.method == 'GET':
         serializer = PuppySerializer(puppy)
         return Response(serializer.data)
+    
+@api_view(['GET', 'POST'])
+def get_post_puppies(request):
+    # get all puppies
+    if request.method == 'GET':
+        puppies = Puppy.objects.all()
+        serializer = PuppySerializer(puppies, many=True)
+        return Response(serializer.data)
+    # insert a new record for a puppy
+    if request.method == 'POST':
+        data = {
+            'name': request.data.get('name'),
+            'age': int(request.data.get('age')),
+            'breed': request.data.get('breed'),
+            'color': request.data.get('color')
+        }
+        serializer = PuppySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def get_delete_update_puppy(request, pk):
+    try:
+        puppy = Puppy.objects.get(pk=pk)
+    except Puppy.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # get details of a single puppy
+    if request.method == 'GET':
+        serializer = PuppySerializer(puppy)
+        return Response(serializer.data)
+
+    # update details of a single puppy
+    if request.method == 'PUT':
+        serializer = PuppySerializer(puppy, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # delete a single puppy
+    elif request.method == 'DELETE':
+        return Response({})
+    
+@api_view(['GET', 'DELETE', 'PUT'])
+def get_delete_update_puppy(request, pk):
+    try:
+        puppy = Puppy.objects.get(pk=pk)
+    except Puppy.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # get details of a single puppy
+    if request.method == 'GET':
+        serializer = PuppySerializer(puppy)
+        return Response(serializer.data)
+
+    # update details of a single puppy
+    if request.method == 'PUT':
+        serializer = PuppySerializer(puppy, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # delete a single puppy
+    if request.method == 'DELETE':
+        puppy.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
