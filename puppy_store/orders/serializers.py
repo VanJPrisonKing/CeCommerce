8635c00c9e72791b4beafa2ad1f5fc13fd2fb6a1
darkVanci <1585781755@qq.com>
2024-05-11 15:27:21 +0800

    change name

--- puppy_store/orders/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Order
-
-
-class PuppySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ('title', 'price', 'description', 'created_at', 'updated_at')
--- puppy_store/puppies/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PuppiesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'puppies'
