bca894d84ff7f3fe2e20e60c8bfd4fcadee51bba
darkVinci <1585781755@qq.com>
2024-05-24 11:31:28 +0800

    delete 2

--- orders/urls.py
+++
@@ -1,9 +0,0 @@
-# orders/urls.py
-from rest_framework.routers import DefaultRouter
-from . import views
-
-router = DefaultRouter()
-router.register("order", views.OrderView)
-router.register("category", views.CategoryView)
-
-urlpatterns = router.urls
